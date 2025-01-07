7139aa0d22e12a774a784e096af7c95d8d71219d
zain6001 <zain756001@gmail.com>
2025-01-07 15:04:08 +0500

    u

---
+++ Question1_WPF/Question1_WPF/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Data;
+using System.Data.SqlTypes;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+
+namespace Question1_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            string query = $"select * from Questions";
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                QuestionsDetails.ItemsSource = table.DefaultView;
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+            }
+        }
+
+        string ConnectionString = "Data Source=DESKTOP-6CLP6M9\\SQLEXPRESS;Initial Catalog=wpfFinal;Integrated Security=True;Trust Server Certificate=True";
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (questionNameTextBox.Text == null || MarksTextBox.Text == null || correctAnswerTextBox.Text == null)
+            {
+                MessageBox.Show("All fields are required");
+                return;
+
+            }
+            string qn=questionNameTextBox.Text;
+            string ca=correctAnswerTextBox.Text;
+           int m = int.Parse(MarksTextBox.Text);
+           
+         
+            string query = $"insert into Questions(questionText,CorrectAnswer,assignedMarks) values ('{qn}','{ca}','{m}')";
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd =new SqlCommand(query,con);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"{ex}");
+            }
+
+            MessageBox.Show("Question Saved Successfully","Success",MessageBoxButton.OK);
+            LoadData();
+        }
+
+        private void SaveQuestion_Click(object sender, RoutedEventArgs e)
+        {
+
+            User newForm = new User();
+            newForm.Show();
+            this.Close();
+
+        }
+    }
+}
---
+++ Question1_WPF/Question1_WPF/User.xaml.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Question1_WPF
+{
+    /// <summary>
+    /// Interaction logic for User.xaml
+    /// </summary>
+    public partial class User : Window
+    {
+        public User()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        string ConnectionString = "Data Source=DESKTOP-6CLP6M9\\SQLEXPRESS;Initial Catalog=wpfFinal;Integrated Security=True;Trust Server Certificate=True";
+
+
+        public void LoadData()
+        {
+            string query = $"select id,QuestionText from Questions";
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                QuestionDataGrid.ItemsSource = table.DefaultView;
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+            }
+        }
+
+        private void SaveQuestion_Click(object sender, RoutedEventArgs e)
+        {
+            int id=int.Parse(qIdTextBox.Text);
+            string answer=answerTextBox.Text;
+            string query = $"Select QuestionText from Questions where id='{id}' and CorrectAnswer='{answer}'";
+            if (id == null || answer == null )
+            {
+                MessageBox.Show("All fields are required");
+                return;
+
+            }
+
+
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                var rows = cmd.ExecuteScalar();
+                if (rows!=null) {
+
+                    int m = int.Parse(Score.Text);
+                    m = m + 1;
+                    Score.Text =m.ToString();
+                    MessageBox.Show("Your answer is correct","Success",MessageBoxButton.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Your answer is incorrect","Warning", MessageBoxButton.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+            }
+
+            LoadData();
+
+        }
+
+        private void QuitQuiz_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow newForm= new MainWindow();  
+            newForm.Show();
+            this.Close();
+        }
+
+        private void Shift_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your dificulty level  is switched", "Success", MessageBoxButton.OK);
+        }
+    }
+}
